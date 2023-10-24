c2baa2cbce81d8877d7b5627ef988ad94c0c209d
KyleRessLiere <kressliere@gmail.com>
2023-10-24 17:33:03 -0400

    added ssn attribute

--- tutorials-master/src/main/java/com/graphqljava/tutorial/bookDetails/Author.java
+++ tutorials-master/src/main/java/com/graphqljava/tutorial/bookDetails/Author.java
@@ -7,17 +7,19 @@ public class Author {
     private String id;
     private String firstName;
     private String lastName;
+    private String ssn;
 
-    public Author(String id, String firstName, String lastName) {
+    public Author(String id, String firstName, String lastName, String ssn) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.ssn = ssn;
     }
 
     private static List<Author> authors = Arrays.asList(
-            new Author("author-1", "Joanne", "Rowling"),
-            new Author("author-2", "Herman", "Melville"),
-            new Author("author-3", "Anne", "Rice")
+            new Author("author-1", "Joanne", "Rowling", "123-45-6789"),
+            new Author("author-2", "Herman", "Melville", "987-65-4321"),
+            new Author("author-3", "Anne", "Rice", "111-22-3333")
     );
 
     public static Author getById(String id) {
@@ -26,5 +28,17 @@ public class Author {
 
     public String getId() {
         return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
     }
 }
